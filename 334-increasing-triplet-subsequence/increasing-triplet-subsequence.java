9e76d373ffbeb553bb4f2a37dc9af586817e8d99
Huynh Minh Khanh <107259188+huynhminhkhanh253@users.noreply.github.com>
2024-02-20 00:54:35 +0700

    Time: 2 ms (95.05%) | Memory: 133.3 MB (85.61%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        if(nums.length < 3) return false;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] <= min1){
+                min1 = nums[i];
+            }
+            else if(nums[i] <= min2){
+                min2 = nums[i];
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
